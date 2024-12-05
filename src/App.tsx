fbbca86e251ec9fa55e3814a530b60c13e21df93
NazarSova <nazarii.sova96@gmail.com>
2024-12-05 14:39:49 +0200

    lesson3/part1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [user, setUser] = useState({});
+  const name = useRef();
+  const age = useRef();
+
+  console.log(user);
+
+  //@ts-ignore
+  const handler = (e) => {
+    e.preventDefault()
+    setUser(prev => ({...prev, [e.target.name]: e.target.value}))
+  }
+
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div>
+      {/* <div>count: {count}</div>
+      <button onClick={() => setCount(prev => prev + 1)}>inc</button> */}
+      <form>
+        <input type='text' placeholder={'name'} name={'name'} onChange={handler}/>
+        <input type='text' placeholder={'age'} name={'age'} onChange={handler}/>
+        <button>Save</button>
+      </form>
     </div>
   );
 }
