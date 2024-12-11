23c8e930bf29ae514283ffeb59f7ed371d0a145d
FerkReden <depper731@gmail.com>
2024-12-11 11:25:26 +0200

    lesson4.hm fix

--- src/pages/PostsPage.tsx
+++ src/pages/PostsPage.tsx
@@ -1,10 +1,21 @@
-import { Posts } from "../components/PostsContainer/Posts";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
+import { IPost } from "../interfaces/postInterface";
+import { postService } from "../services/postsService";
+import { Post } from "../components/PostsContainer/Post";
 
 const PostPage = () => {
 
+    const [post, setPost] = useState<IPost>();
+    const { state: { postId } } = useLocation();
+
+    useEffect(() => {
+        postService.getById(postId).then(({data}) => setPost(data))
+    }, [postId])
     return (
         <div>
-            <Posts />
+            {post && <Post key={post.id} post={post}/>}
         </div>
     )
 };
